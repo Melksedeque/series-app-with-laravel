630d75b5fe52cfd28e404becbedcd6d93a240225
Melksedeque Silva <melksedeque.silva@globalpharmaservices.com>
2023-07-28 17:10:25 -0300

    HTML Structure of Series

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,21 @@
-<section class="series">
-    <ul>
-        @forelse ($series as $serie)
-            <li>{{$serie}}</li>
-        @empty
-            <p>Não foi possível encontrar nenhuma série</p>
-        @endforelse
-    </ul>
-</section>
+<x-layout title="Series">
+    <header class="site-header">
+        <div class="container">
+            <h1>{{ $title }}</h1>
+        </div>
+    </header>
+
+    <section class="series">
+        <div class="container">
+            <ul>
+                @forelse ($series as $serie)
+                    <li>{{$serie}}</li>
+                @empty
+                    <p>Não foi possível encontrar nenhuma série</p>
+                @endforelse
+            </ul>
+        </div>
+    </section>
+
+    <footer class="site-footer"></footer>
+</x-layout>
