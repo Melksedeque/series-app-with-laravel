1917fa5d7bfebe3d9da82b3233d26463fd18acb3
Melksedeque Silva <melksedeque.silva@globalpharmaservices.com>
2023-07-28 22:39:54 -0300

    Series Controller - edit() done; update() in progress;

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,17 +6,19 @@ use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+use function PHPUnit\Framework\returnSelf;
+
 class SeriesController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::all()->sortBy('title');
-        // dd($series);
+        $message = session('success.message');
 
-        return view('series.index', compact('series'));
+        return view('series.index', compact('series'))->with('message', $message);
     }
 
     /**
@@ -32,9 +34,9 @@ class SeriesController extends Controller
      */
     public function store(Request $request)
     {
-        Serie::create($request->all());
+        $serie = Serie::create($request->all());
 
-        return to_route('series.index');
+        return to_route('series.index')->with('success.message', "Série <strong>'{$serie->title}'</strong> adicionada com sucesso!");
     }
 
     /**
@@ -48,24 +50,30 @@ class SeriesController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Serie $serie)
+    public function edit(Serie $series)
     {
-        //
+        return view('series.edit')->with('series', $series);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Serie $serie)
+    public function update(Request $request, Serie $series)
     {
-        //
+        $series->fill($request->all());
+        dd($series);
+        $series->save();
+
+        return to_route('series.index')->with('success.message', "Série '{$series->title} atualizada com sucesso!'");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Serie $serie)
+    public function destroy(Serie $series)
     {
-        //
+        $series->delete();
+
+        return to_route('series.index')->with('success.message', "Série '{$series->title}' removida com sucesso!");
     }
 }
