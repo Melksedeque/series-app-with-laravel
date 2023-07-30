4033e1f1266c4797bf598094d2d19c33dfd608a7
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-07-29 21:01:21 -0300

    Include the CSS and JS files through Vite in head

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/estilos.css') }}">
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
