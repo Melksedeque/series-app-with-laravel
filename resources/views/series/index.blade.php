f00bb357b24de2aaea8b585eba5b6e33e8a4182e
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-07-30 12:44:34 -0300

    Including FontAwesome Icons in buttons

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,7 +7,7 @@
     <a href="{{ route('series.create') }}" class="btn btn-success">+ Adicionar</a>
 
     @isset($message)
-        <div class="alert alert-success w-100">{{ $message }}</div>
+        <div class="alert alert-success mt-4 w-100">{{ $message }}</div>
     @endisset
 </header>
 <section class="series">
@@ -15,12 +15,16 @@
         @forelse ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 {{ $serie->title }}
-                <div class="options d-flex justify-content-around">
-                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">Ed</a>
+                <div class="options d-flex justify-content-around gap-3">
+                    <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </a>
                     <form action="{{ route('series.destroy', $serie->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-danger btn-sm">X</button>
+                        <button class="btn btn-danger btn-sm">
+                            <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
                     </form>
                 </div>
             </li><br><br>
