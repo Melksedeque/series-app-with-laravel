68068a1165521edaede00afd0147ca2a834d0545
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-07-30 12:18:20 -0300

    Creating a master layout to be imported

---
+++ resources/views/site/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Series Controller</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,700;1,300;1,700&display=swap" rel="stylesheet">
+    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <script src={{ asset('js/app.js') }}></script>
+</body>
+
+</html>
