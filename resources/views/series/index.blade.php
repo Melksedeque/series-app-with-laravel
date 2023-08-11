e33cba5780914b470a4872cda2835fce3a091d7c
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-11 10:42:16 -0300

    Criação de botões de show, edit e delete series em cada li

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -4,7 +4,14 @@
     <ul class="list-group">
         @foreach ($series as $serie)
         {{-- {{dd($serie)}} --}}
-        <li class="list-group-item">{{ $serie->title }} <a class="btn btn-primary" href="{{ route('series.edit', $serie->id) }}">Editar</a></li>
+        <li class="list-group-item">
+            {{ $serie->title }}
+            <div class="options">
+                <a class="btn btn-success" href="{{ route('series.show', $serie->id) }}"><i class="bi bi-eye"></i></a>
+                <a class="btn btn-primary" href="{{ route('series.edit', $serie->id) }}"><i class="bi bi-pencil-square"></i></a>
+                <a class="btn btn-danger" href="{{ route('series.destroy', $serie->id) }}"><i class="bi bi-trash"></i></a>
+            </div>
+        </li>
         @endforeach
     </ul>
 </x-layout>
