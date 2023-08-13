fbba88d2625098add03b82b367c0ff484c7f3af8
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-12 23:27:14 -0300

    Target blank in link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="site-footer bg-secondary text-white text-center mt-5 p-2">
-    <p>Desenvolvido por <a href="https://www.github.com/Melksedeque">Melksedeque</a></p>
+    <p>Desenvolvido por <a href="https://www.github.com/Melksedeque" target="_blank">Melksedeque</a></p>
 </footer>
