3bcb3f1be10bc9ab31c20f6f1bc79a1874158f31
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-15 19:15:37 -0300

    Mensagem de erro adicionada no layout padrão

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,6 +13,15 @@
     @include('components.header')
 
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         {{ $slot }}
     </div>
 
