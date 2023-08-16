3fc011bfdb403d8e169254ef81a952fd1e5cf63d
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-16 11:18:02 -0300

    Adição de bootstrap-icon no alert error

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,16 +12,17 @@
 <body>
     @include('components.header')
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li><i class="bi bi-x-octagon-fill"></i> {{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <div class="container">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         {{ $slot }}
     </div>
 
