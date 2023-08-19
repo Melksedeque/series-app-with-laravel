bd66fcc18f781b8b748730f30fda74e235460ca0
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-19 16:17:08 -0300

    Conclusão do metodo store de series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
+use App\Models\Episode;
+use App\Models\Season;
 use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -36,17 +38,37 @@ class SeriesController extends Controller
      */
     public function store(SeriesFormRequest $request)
     {
-        $data = $request->all();
-        // dd($serie["image"]);
-        if ($request->hasFile('image') && $request->image->isValid()) {
-            $data["image"] = $request->image->store('series', 'public');
-        }
+        $serie = DB::transaction(function () use ($request) {
+            $serieData = $request->except('seasons');
+            $serie = Serie::create($serieData);
 
-        $serie = Serie::create($data);
-        // dd($serie);
+            if ($request->has('seasons') && is_array($request->input('seasons'))) {
+                foreach ($request->input('seasons') as $season => $episodes) {
+                    $season = new Season([
+                        'number' => $season + 1,
+                    ]);
+
+                    $serie->seasons()->save($season);
+
+                    if (isset($episodes) && is_array($episodes)) {
+                        foreach ($episodes as $episode) {
+                            $episode = new Episode([
+                                'number' => $episode,
+                            ]);
+
+                            $season->episodes()->save($episode);
+                        }
+                    }
+                }
+            }
+
+            return $serie;
+        });
+
         $text = "Série '$serie->title' adicionada com sucesso!";
         return redirect()->route('serie.index')->with('success.message', $text);
     }
+
 
     /**
      * Display the specified resource.
