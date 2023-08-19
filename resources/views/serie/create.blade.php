afcf70c1627576eead2d7db4fa64752e7f7a4da2
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-08-19 16:19:27 -0300

    Finalização da estrutura de formulário de cadastro de séries

--- resources/views/serie/create.blade.php
+++ resources/views/serie/create.blade.php
@@ -5,39 +5,44 @@
 
     <form action="{{ route('serie.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="mb-3">
-            <label for="title" class="form-label">Título da Série: <span class="text-danger">*</span></label>
-            <input type="text" class="form-control"
-            name="title"
-            value="{{ old('title') }}"
-            placeholder="Título da série"
-            autofocus>
+        <div class="left">
+            <div class="mb-3">
+                <label for="title" class="form-label">Título da Série: <span class="text-danger">*</span></label>
+                <input type="text" class="form-control"
+                name="title"
+                value="{{ old('title') }}"
+                placeholder="Título da série"
+                autofocus>
+            </div>
+            <div class="mb-3">
+                <label for="premiere_date" class="form-label">Data de lançamento:</label>
+                <input type="date" class="form-control"
+                name="premiere_date"
+                value="{{ old('premiere_date') }}"
+                placeholder="Data de Estréia">
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Pôster:</label>
+                <input type="file" class="form-control"
+                    name="image">
+            </div>
+            <div class="mb-3">
+                <label for="synopsis" class="form-label">Sinopse:</label>
+                <textarea name="synopsis" class="form-control"
+                cols="30" rows="5"
+                placeholder="Sinopse">{{ old('synopsis') }}</textarea>
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="seasons" class="form-label">Quantas temporadas possui:</label>
-            <input type="text" class="form-control"
-            name="seasons"
-            value="{{ old('seasons') }}"
-            placeholder="Quantidade de Temporadas">
+        <div class="right">
+            <div class="seasons-container d-flex flex-row flex-wrap justify-content-between align-items-center">
+                <h4>Temporadas e Episódios</h4>
+                <button type="button" class="btn btn-sm btn-outline-success" id="add-season" data-button="add-season">Adicionar Temporada</button>
+                <hr class="w-100">
+            </div>
         </div>
-        <div class="mb-3">
-            <label for="premiere_date" class="form-label">Data de lançamento:</label>
-            <input type="date" class="form-control"
-            name="premiere_date"
-            value="{{ old('premiere_date') }}"
-            placeholder="Data de Estréia">
+        <div class="w-100">
+            <button type="submit" class="btn btn-primary">Salvar</button>
         </div>
-        <div class="mb-3">
-            <label for="synopsis" class="form-label">Sinopse:</label>
-            <textarea name="synopsis" class="form-control"
-            cols="30" rows="5"
-            placeholder="Sinopse">{{ old('synopsis') }}</textarea>
-        </div>
-        <div class="mb-3">
-            <input type="file" class="form-control"
-                name="image">
-        </div>
-        <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 
 </x-layout>
