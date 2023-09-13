3ddc2829271374ecfbd0b7bd6ed57dd171bfc7bb
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-13 16:27:13 -0300

    FormRequest de series com autenticação

--- app/Http/Requests/SeriesFormRequest.php
+++ app/Http/Requests/SeriesFormRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class SeriesFormRequest extends FormRequest
 {
@@ -11,7 +12,10 @@ class SeriesFormRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        if(Auth::check()) {
+            return true;
+        }
+        return false;
     }
 
     /**
